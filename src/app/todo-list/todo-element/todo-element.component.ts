de676b6db140e7a787ed4633881e8ddc92733f66
louisa <lmoller@procentec.com>
2022-02-24 16:12:55 +0100

    Added more rxjs & styling fix

--- src/app/todo-list/todo-element/todo-element.component.ts
+++ src/app/todo-list/todo-element/todo-element.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { TaskModel } from 'src/app/shared/general.model';
+import { fromEvent } from 'rxjs'
 
 @Component({
   selector: 'app-todo-element',
   templateUrl: './todo-element.component.html',
   styleUrls: ['./todo-element.component.scss']
 })
-export class TodoElementComponent implements OnInit {
+export class TodoElementComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
@@ -17,6 +18,14 @@ export class TodoElementComponent implements OnInit {
 
   doneTask(t:TaskModel): void {
     t.done = !t.done;
+  }
+
+  ngAfterViewInit(): void {
+    const checkbox = document.getElementById("task") as HTMLElement;
+
+    const myObservable = fromEvent(checkbox, 'click');
+  
+    const subscription = myObservable.subscribe(event => this.doneTask(this.task));
   }
 
 }
